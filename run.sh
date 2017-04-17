567dc29df73d91ea7050f2247348f8853673f22d
Andrew Salveson <andrewsalveson@gmail.com>
2017-04-17 18:18:20 +0000

    local changes

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ docker rm geoserver
 docker kill geoserver-postgis
 docker rm geoserver-postgis
 
-DATA_DIR=~/geoserver_data
+DATA_DIR=/var/geoserver
 if [ ! -d $DATA_DIR ]
 then
     mkdir -p $DATA_DIR
@@ -16,6 +16,6 @@ docker run \
 	--name=geoserver \
 	--link geoserver-postgis:postgis \
         -v $DATA_DIR:/opt/geoserver/data_dir \
-	-p 8080:8080 \
+	-p $1:8080 \
 	-d \
 	-t kartoza/geoserver
